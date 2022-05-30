08ee8d9c3682f3cd1717f6dec3fb84be5a65bebe
Nkosi Yafeu <95482668+Supremedesignn@users.noreply.github.com>
2022-05-30 13:40:29 -0400

    Initial Commit

---
+++ Animate Course/Animate_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animate_CourseApp.swift
+//  Animate Course
+//
+//  Created by Nkosi Yafeu on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Animate_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
