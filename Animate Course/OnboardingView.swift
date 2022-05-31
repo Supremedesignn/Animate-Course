16bbcf6e53eb8e33540b45b84f036522c3d2ed0f
Nkosi Yafeu <95482668+Supremedesignn@users.noreply.github.com>
2022-05-30 20:29:44 -0400

    Button Animation with Rive

--- Animate Course/OnboardingView.swift
+++ Animate Course/OnboardingView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "button", autoPlay: false)
+    
     var body: some View {
+        ZStack {
+            background
+   
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start the course", systemImage: "arrow.forward")
+                        .offset(x: 4, y: 4)
+                        .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10) 
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+            
+                .onTapGesture {
+                     button.play(animationName: "active")
+                }
+        }
+    }
+    
+    var background: some View {
         RiveViewModel(fileName: "shapes").view()
             .ignoresSafeArea()
             .blur(radius: 30)
@@ -17,7 +44,7 @@ struct OnboardingView: View {
                  Image("")
                     .blur(radius: 50)
                     .offset(x: 200, y: 100)
-            )
+        )
     }
 }
 
